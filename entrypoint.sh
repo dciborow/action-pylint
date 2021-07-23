cd118eef6fa15e33b5bea7aec15c399e97d0470a
Daniel Ciborowski <dciborow@microsoft.com>
2021-07-22 22:33:06 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,9 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-misspell -locale="${INPUT_LOCALE}" . \
+pylint --rcfile="${INPUT_CONFIG}" . \
   | reviewdog -efm="%f:%l:%c: %m" \
-      -name="linter-name (misspell)" \
+      -name="linter-name (pylint)" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
