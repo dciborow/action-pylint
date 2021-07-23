d5416036aa5b6485f247999113ef33909dabfedf
Daniel Ciborowski <dciborow@microsoft.com>
2021-07-22 22:48:08 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,38 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+set -eu # Increase bash strictness
 
-if [ -n "${GITHUB_WORKSPACE}" ] ; then
+if [[ -n "${GITHUB_WORKSPACE}" ]]; then
   cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
 fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-pylint --rcfile=.pylintrc . \
-  | reviewdog -efm="%f:%l:%c: %m" \
-      -name="linter-name (pylint)" \
-      -reporter="${INPUT_REPORTER:-github-pr-check}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-      -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
+export REVIEWDOG_VERSION=v0.13.0
+
+echo "[action-flake8] Installing reviewdog..."
+wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b /tmp "${REVIEWDOG_VERSION}"
+
+if [[ "$(which pylint)" == "" ]]; then
+  echo "[action-flake8] Installing pylint package..."
+  python -m pip install --upgrade pylint
+fi
+echo "[action-pylint] pylint version:"
+pylint --version
+
+echo "[action-pylint] Checking python code with the pylint linter and reviewdog..."
+exit_val="0"
+pylint --rc-file .pylintrc . 2>&1 | # Removes ansi codes see https://github.com/reviewdog/errorformat/issues/51
+  /tmp/reviewdog -f=pylint \
+    -name="${INPUT_TOOL_NAME}" \
+    -reporter="${INPUT_REPORTER}" \
+    -filter-mode="${INPUT_FILTER_MODE}" \
+    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+    -level="${INPUT_LEVEL}" \
+    ${INPUT_REVIEWDOG_FLAGS} || exit_val="$?"
+
+echo "[action-pylint] Clean up reviewdog..."
+rm /tmp/reviewdog
+
+if [[ "${exit_val}" -ne '0' ]]; then
+  exit 1
+fi
