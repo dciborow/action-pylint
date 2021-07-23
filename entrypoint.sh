06635e6a00651d77b8b467ccea933da375037510
Daniel Ciborowski <dciborow@microsoft.com>
2021-07-22 22:56:14 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ pylint --version
 echo "[action-pylint] Checking python code with the pylint linter and reviewdog..."
 exit_val="0"
 pylint --rcfile ../.pylintrc . 2>&1 | # Removes ansi codes see https://github.com/reviewdog/errorformat/issues/51
-  /tmp/reviewdog -f=pylint \
+  /tmp/reviewdog -efm="%f:%l:%c: %m" \
     -name="${INPUT_TOOL_NAME}" \
     -reporter="${INPUT_REPORTER}" \
     -filter-mode="${INPUT_FILTER_MODE}" \
