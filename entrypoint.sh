7ff1b618cbd8a6eb09f74b699d9cd057a4d678c7
Justin Chu <justinchuby@users.noreply.github.com>
2022-05-14 10:12:39 -0700

    Review dog

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,16 +31,14 @@ fi
 echo "[action-pylint] Checking python code with the pylint linter and reviewdog..."
 exit_val="0"
 
-pylint --score n ${rcfile_option} ${INPUT_PYLINT_ARGS} ${INPUT_GLOB_PATTERN} 2>&1
-
-#  | # Removes ansi codes see https://github.com/reviewdog/errorformat/issues/51
-#   /tmp/reviewdog -efm="%f:%l:%c: %m" \
-#     -name="${INPUT_TOOL_NAME}" \
-#     -reporter="${INPUT_REPORTER}" \
-#     -filter-mode="${INPUT_FILTER_MODE}" \
-#     -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-#     -level="${INPUT_LEVEL}" \
-#     ${INPUT_REVIEWDOG_FLAGS} || exit_val="$?"
+pylint --score n ${rcfile_option} ${INPUT_PYLINT_ARGS} ${INPUT_GLOB_PATTERN} 2>&1 | # Removes ansi codes see https://github.com/reviewdog/errorformat/issues/51
+  /tmp/reviewdog -efm="%f:%l:%c: %m" \
+    -name="${INPUT_TOOL_NAME}" \
+    -reporter="${INPUT_REPORTER}" \
+    -filter-mode="${INPUT_FILTER_MODE}" \
+    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+    -level="${INPUT_LEVEL}" \
+    ${INPUT_REVIEWDOG_FLAGS} || exit_val="$?"
 
 echo "[action-pylint] Clean up reviewdog..."
 rm /tmp/reviewdog
