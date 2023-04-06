bb8afe97c00b554fd02a9d92bd214c1cfcf5e033
WU MENGHUNG <wu.menghung@jp.panasonic.com>
2023-04-06 17:42:25 +0900

    add debug msg

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,6 +30,12 @@ fi
 echo "[action-pylint] Checking python code with the pylint linter and reviewdog..."
 exit_val="0"
 
+echo "rcfile_option=${rcfile_option}"
+echo "INPUT_PYLINT_ARGS=${INPUT_PYLINT_ARGS}"
+echo "INPUT_GLOB_PATTERN=${INPUT_GLOB_PATTERN}"
+
+pylint --score n ${rcfile_option} ${INPUT_PYLINT_ARGS} ${INPUT_GLOB_PATTERN} 2>&1
+
 pylint --score n ${rcfile_option} ${INPUT_PYLINT_ARGS} ${INPUT_GLOB_PATTERN} 2>&1 | # Removes ansi codes see https://github.com/reviewdog/errorformat/issues/51
   /tmp/reviewdog -efm="%f:%l:%c: %m" \
     -name="${INPUT_TOOL_NAME}" \
